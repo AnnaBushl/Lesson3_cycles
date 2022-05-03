ce3d394af8441b3bc084b72e6940d86caae5ab1a
Ann <ann.bushl2444@gmail.com>
2022-05-03 14:43:29 +0300

    Задача 2

--- src/task2.java
+++ src/task2.java
@@ -1,6 +1,6 @@
 public class task2 {
     public static void main(String[] args) {
-        int Fr = 1;  // Можо изменять число первой пятницы от 1 до 7
+        int Fr = 1;  // Можно изменять число первой пятницы от 1 до 7
         while (Fr <= 31){
             System.out.println("Сегодня пятница, " + Fr + "-е число. Необходимо подготовить отчет.");
             Fr = Fr + 7;
